4bda8dfd2f51d29b1baf5e209a6c0bde5718b600
root <xipeng>
2019-02-19 11:29:47 +0800

    add bitree interface

--- test_bitree.cc
+++ test_bitree.cc
@@ -44,6 +44,7 @@ void add_tree(binode **p) {
     add_tree(& ((*p)->right));
 }
 
+
 void print_tree(binode *root) {
     if (root == NULL) return;
     printf("%c", root->val);
@@ -51,12 +52,36 @@ void print_tree(binode *root) {
     print_tree(root->right);
 }
 
+int tree_depth(binode *root) {
+    if (root == NULL) return 0;
+    
+    int left_depth = tree_depth(root->left);
+    int right_depth = tree_depth(root->right);
+
+    return left_depth > right_depth ? left_depth + 1 : right_depth + 1;
+}
+int tree_node_sum(binode *root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    int left_sum = tree_node_sum(root->left);
+    int right_sum = tree_node_sum(root->right);
+    return left_sum + right_sum + 1;
+
+}
 int main() {
     binode *root;
     add_tree(&root);
     printf("..........print tree begin....\n");
     print_tree(root);
     printf("\n..........print tree end....\n");
+
+    int depth = tree_depth(root);
+    printf("depth=%d\n", depth);
+    int sum = tree_node_sum(root);
+    printf("node sum=%d\n", sum);
+    return 0;
 }
 
 
